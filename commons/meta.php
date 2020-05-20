11ec54c0024bf3e6a25cdb3b6e425508e34225ed
afa28 <andifahruddinakas@gmail.com>
2020-05-21 05:11:45 +0700

    Sesuai aturan penulisan .js

--- commons/meta.php
+++ commons/meta.php
@@ -33,28 +33,30 @@
 	<link rel="stylesheet" href="<?= base_url("$this->theme_folder/$this->theme/assets/css/style.css"); ?>">
 	<link rel="stylesheet" href="<?= base_url("$this->theme_folder/$this->theme/assets/css/custom.css"); ?>">
 
-<!-- nambah baru lagi
-	<link type='text/css' href="<?= base_url()?>Xassets/bootstrap/css/bootstrap.min.css" rel='stylesheet' />-->
+	<!-- nambah baru lagi
+	<link rel="stylesheet" href="<?= base_url()?>Xassets/bootstrap/css/bootstrap.min.css" />-->
 	<link rel="stylesheet" href="<?= base_url()?>assets/css/font-awesome.min.css" />
 	<link rel="stylesheet" href="<?= base_url()?>assets/css/leaflet.css" />
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+	<link rel="stylesheet" href="<?= base_url() ?>assets/bootstrap/css/dataTables.bootstrap.min.css">
+
 	<?php if(isset($single_artikel)): ?>
-		<meta name='description' content="<?= $single_artikel["judul"];?>"/>
+		<meta name="description" content="<?= $single_artikel["judul"];?>"/>
 		<meta property="og:title" content="<?= $single_artikel["judul"];?>"/>
 		<meta property="og:url" content="<?= site_url('artikel/'.buat_slug($single_artikel))?>"/>
 		<meta property="og:image" content="<?= base_url()?><?= LOKASI_FOTO_ARTIKEL?>sedang_<?= $single_artikel['gambar'];?>"/>
 		<meta property="og:description" content="<?= potong_teks($single_artikel['isi'], 300)?> ..."/>
 	<?php else: ?>
-		<meta name='description' content="<?= ucwords($this->setting->sebutan_desa).' '.$desa['nama_desa'].' '.ucwords($this->setting->sebutan_kecamatan).' '.$desa['nama_kecamatan'].' '.ucwords($this->setting->sebutan_kabupaten).' '.$desa['nama_kabupaten'];?>"/>
+		<meta name="description" content="<?= ucwords($this->setting->sebutan_desa).' '.$desa['nama_desa'].' '.ucwords($this->setting->sebutan_kecamatan).' '.$desa['nama_kecamatan'].' '.ucwords($this->setting->sebutan_kabupaten).' '.$desa['nama_kabupaten'];?>"/>
 		<meta property="og:title" content="<?= ucwords($this->setting->sebutan_desa).' '.$desa['nama_desa'].' '.ucwords($this->setting->sebutan_kecamatan).' '.$desa['nama_kecamatan'].' '.ucwords($this->setting->sebutan_kabupaten).' '.$desa['nama_kabupaten'];?>"/>
 		<meta property="og:url" content="<?= site_url()?>"/>
 		<!--<meta property="og:image" content="<?= base_url("$this->theme_folder/$this->theme/images/header.jpg"); ?>"/>-->
 		<meta property="og:description" content="<?= $this->setting->website_title.' '.ucwords($this->setting->sebutan_desa).' '.$desa['nama_desa'].' '.ucwords($this->setting->sebutan_kecamatan).' '.$desa['nama_kecamatan'].' '.ucwords($this->setting->sebutan_kabupaten).' '.$desa['nama_kabupaten'];?>"/>
 	<?php endif; ?>
 		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ if (window.scrollY == 0) window.scrollTo(0,1); } </script>
-		<script language='javascript' src="<?= base_url('assets/front/js/jquery.min.js'); ?>"></script>
-		<script language='javascript' src="<?= base_url('assets/front/js/jquery.cycle2.min.js') ?>"></script>
-		<script language='javascript' src="<?= base_url('assets/front/js/jquery.cycle2.carousel.js') ?>"></script>
+		<script src="<?= base_url('assets/front/js/jquery.min.js'); ?>"></script>
+		<script src="<?= base_url('assets/front/js/jquery.cycle2.min.js') ?>"></script>
+		<script src="<?= base_url('assets/front/js/jquery.cycle2.carousel.js') ?>"></script>
 		<script src="<?= base_url("$this->theme_folder/$this->theme/assets/js/bootstrap.min.js") ?>"></script>
 		<script src="<?= base_url()?>assets/js/leaflet.js"></script>
 
@@ -64,8 +66,6 @@
 		<script src="<?= base_url()?>assets/js/highcharts/highcharts.js"></script>
 		<script src="<?= base_url()?>assets/js/highcharts/exporting.js"></script>
 		<script src="<?= base_url()?>assets/js/highcharts/highcharts-more.js"></script>
-
-		<link rel="stylesheet" href="<?= base_url() ?>assets/bootstrap/css/dataTables.bootstrap.min.css">
 		<script src="<?= base_url() ?>assets/bootstrap/js/jquery.dataTables.min.js"></script>
 		<script src="<?= base_url() ?>assets/bootstrap/js/dataTables.bootstrap.min.js"></script>
 		<script type="text/javascript">
@@ -78,43 +78,49 @@
 			window.setTimeout("renderDate()",1);
 			days = new Array("Minggu","Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu");
 			months = new Array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember");
-			function renderDate()
-			{
+
+			function renderDate() {
 				var mydate = new Date();
 				var year = mydate.getYear();
-				if (year < 2000)
-				{
+
+				if (year < 2000) {
 					if (document.all)
 						year = "19" + year;
 					else
 						year += 1900;
 				}
+
 				var day = mydate.getDay();
 				var month = mydate.getMonth();
 				var daym = mydate.getDate();
+
 				if (daym < 10)
 					daym = "0" + daym;
+
 				var hours = mydate.getHours();
 				var minutes = mydate.getMinutes();
 				var seconds = mydate.getSeconds();
+
 				if (hours <= 9)
 					hours = "0" + hours;
 				if (minutes <= 9)
 					minutes = "0" + minutes;
 				if (seconds <= 9)
 					seconds = "0" + seconds;
+
 				document.getElementById("jam").innerHTML = "<B>"+days[day]+", "+daym+" "+months[month]+" "+year+"</B><br>"+hours+" : "+minutes+" : "+seconds;
 				setTimeout("renderDate()",1000)
 			}
 		</script>
 		<style type="text/css">
-			#jam{
+			#jam {
 				background: #000000;
 				text-align:center;
 				text-decoration: blink;
 				margin-top:0,5%;
 				color: #ffffff
-			</style><div id="fb-root"></div>
+		</style>
+		<div id="fb-root"></div>
 			<script async defer crossorigin="anonymous" src="https://connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v3.2&appId=147912828718&autoLogAppEvents=1"></script>
 <!--[if lt IE 9]>
 <script src="<?= base_url("$this->theme_folder/$this->theme/assets/js/html5shiv.min.js"); ?>"></script>
