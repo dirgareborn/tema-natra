9747162c68aa1f6632846515212590c77f540465
afa28 <andifahruddinakas@gmail.com>
2020-05-23 09:04:13 +0700

    Penyesuaian template dan menu

--- template.php
+++ template.php
@@ -2,9 +2,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <?php $this->load->view("$folder_themes/commons/meta.php"); ?>
-    <!-- </head> -->
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<?php $this->load->view("$folder_themes/commons/meta.php"); ?>
+	<!-- </head> -->
 </head>
 <body onLoad="renderDate()">
 <!--
@@ -14,31 +14,32 @@
 -->
 <a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
 <div class="container" style="background-color: #f6f6f6;">
-    <header id="header">
-        <?php $this->load->view("$folder_themes/partials/header.php"); ?>
-    </header>
-    <div id="navarea">
-        <?php $this->load->view("$folder_themes/partials/menu_head.php"); ?>
-    </div>
-    <div class="row">
-        <section id="mainContent">
-            <div class="content_middle"></div>
-            <div class="content_bottom">
-                <div class="col-lg-9 col-md-9">
-                    <?php $this->load->view("$folder_themes/partials/bottom_content_left.php"); ?>
-                </div>
-                <div class="col-lg-3 col-md-3">
-                    <?php $this->load->view("$folder_themes/partials/bottom_content_right.php"); ?>
-                </div>
-            </div>
-        </section>
-    </div>
+	<header id="header">
+		<?php $this->load->view("$folder_themes/partials/header.php"); ?>
+	</header>
+	<div id="navarea">
+		<?php $this->load->view("$folder_themes/partials/menu_head.php"); ?>
+	</div>
+	<div class="row">
+		<section id="mainContent">
+			<div class="content_middle"></div>
+			<div class="content_bottom">
+				<div class="col-lg-9 col-md-9">
+					<?php $this->load->view("$folder_themes/partials/bottom_content_left.php"); ?>
+				</div>
+				<div class="col-lg-3 col-md-3">
+					<?php $this->load->view("$folder_themes/partials/bottom_content_right.php"); ?>
+				</div>
+			</div>
+		</section>
+	</div>
 </div>
 <footer id="footer">
-<?php $this->load->view("$folder_themes/partials/footer_top.php"); ?>
-<?php $this->load->view("$folder_themes/partials/footer_bottom.php"); ?>
+<?php
+	$this->load->view("$folder_themes/partials/footer_top.php");
+	$this->load->view("$folder_themes/partials/footer_bottom.php");
+?>
 </footer>
 <?php $this->load->view("$folder_themes/commons/meta_footer.php"); ?>
-<!-- </body></html> -->
 </body>
 </html>
